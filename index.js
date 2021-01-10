e1fd2d74f683c2a4961b6d9a13aa436bfde8a14f
Muhammad Tiyas Fachreza Akbar <tiyas.akbar@gmail.com>
2021-01-11 00:38:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./api/router");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/v1", router);
+
+app.get("/", (req, res) => res.send("Hello okhome!"));
+
+app.listen(port, () => {
+  console.log(`Server are running on port ${port}`);
+});
